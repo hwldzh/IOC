8357c527d56b6e90cc2f515097bfb03492e22a01
hwldzh <821187649@qq.com>
2017-07-28 01:58:22 +0800

    提交代码

--- TestIOC/app/src/main/java/com/tencent/mm/wxperformancetool/testioc/view/ViewInjector.java
+++ TestIOC/app/src/main/java/com/tencent/mm/wxperformancetool/testioc/view/ViewInjector.java
@@ -1,8 +1,11 @@
 package com.tencent.mm.wxperformancetool.testioc.view;
 
+import android.app.Activity;
+
 /**
  * Created by RunningH on 2017/7/28.
  */
 
 public interface ViewInjector {
+    void inject(Activity activity);
 }
